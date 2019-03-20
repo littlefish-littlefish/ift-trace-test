c1087ecdb0e5290753601f4ea14721c7f2e64ebd
littlefish-littlefish <yichongyu@hotmail.com>
2019-03-19 22:32:10 -0400

    update ui

--- routes/index.js
+++ routes/index.js
@@ -28,43 +28,58 @@ router.get('/trace', function(req, res) {
     "facility_name": "Farm1",
     "facility_type": "Farm",
     "facility_address": "1 Farm Street, Gouves, Gouves, GR",
-    "timestamp": "2018-03-01T10:00:00Z"
+    "timestamp": "2018-03-01T10:00:00Z",
+    "image": "images/farm1.png",
+    "event": "Harvest tomato"
   }, {
     "_id"  :  "CCH0001-0002", 
     "facility_name": "Farm2",
     "facility_type": "Farm",
     "facility_address": "1 Farm Street, Bergamo, Lombardy, IT",
-    "timestamp": "2018-03-01T11:00:00Z"
+    "timestamp": "2018-03-01T11:00:00Z",
+    "image": "images/farm2.png",
+    "event": "Harvest tomato"
   }, {
     "_id"  :  "CCH0001-0003", 
     "facility_name": "Farm3",
     "facility_type": "Farm",
     "facility_address": "1 Farm Street, Amstelveen, Amstelveen, NL",
-    "timestamp": "2018-03-01T12:00:00Z"
+    "timestamp": "2018-03-01T12:00:00Z",
+    "image": "images/farm3.png",
+    "event": "Harvest tomato"
   }, {
     "_id"  :  "CCH0001-0004", 
     "facility_name": "Farm4",
     "facility_type": "Farm",
     "facility_address": "1 Farm Street, Fuenteheridos, Huelva, ES",
-    "timestamp": "2018-03-01T13:00:00Z"
+    "timestamp": "2018-03-01T13:00:00Z",
+    "image": "images/farm4.png",
+    "event": "Harvest tomato"
   }, {
     "_id"  :  "CCH0001-0005", 
     "facility_name": "Concentrate Plant 1",
     "facility_type": "Manufactureing plant",
     "facility_address": "1 Main Street, Prosotsani, Prosotsani, GR",
-    "timestamp": "2018-03-04T10:00:00Z"
+    "timestamp": "2018-03-04T10:00:00Z",
+    "image": "images/ConcentratePlant1.png",
+    "event": "Produce concentrate"
+
   }, {
     "_id"  :  "CCH0001-0006", 
     "facility_name": "Reseller Warehouse 1",
     "facility_type": "Warehouse and or depot",
     "facility_address": "1 Main Street, Prosotsani, Prosotsani, GR",
-    "timestamp": "2018-03-08T11:00:00Z"
+    "timestamp": "2018-03-08T11:00:00Z",
+    "image": "images/ConcentratePlant2.png",
+    "event": "Produce concentrate"
   }, {
     "_id"  :  "CCH0001-0007", 
     "facility_name": "Manufacture Plant 1",
     "facility_type": "Manufacturing plant",
     "facility_address": "1 Main Street, Prosotsani, Prosotsani, GR",
-    "timestamp": "2018-03-11T10:00:00Z"
+    "timestamp": "2018-03-11T10:00:00Z",
+    "image": "images/TomatoJuicePlant1.png",
+    "event": "Produce juice"
   }];
 
   var trace2 = [{ 
@@ -72,25 +87,33 @@ router.get('/trace', function(req, res) {
     "facility_name": "Farm3",
     "facility_type": "Farm",
     "facility_address": "1 Farm Street, Amstelveen, Amstelveen, NL",
-    "timestamp": "2018-03-01T12:00:00Z"
+    "timestamp": "2018-03-01T12:00:00Z",
+    "image": "images/farm3.png",
+    "event": "Harvest tomato"
   }, {
     "_id"  :  "CCH0001-0002", 
     "facility_name": "Farm4",
     "facility_type": "Farm",
     "facility_address": "1 Farm Street, Fuenteheridos, Huelva, ES",
-    "timestamp": "2018-03-01T13:00:00Z"
+    "timestamp": "2018-03-01T13:00:00Z",
+    "image": "images/farm4.png",
+    "event": "Harvest tomato"
   }, {
     "_id"  :  "CCH0001-0003", 
     "facility_name": "Concentrate Plant 2",
     "facility_type": "Manufactureing plant",
     "facility_address": "1 Main Street, Zaragoza, Zaragoza, ES",
-    "timestamp": "2018-03-04T12:00:00Z"
+    "timestamp": "2018-03-04T12:00:00Z",
+    "image": "images/ConcentratePlant2.png",
+    "event": "Produce concentrate"
   }, {
     "_id"  :  "CCH0001-0004", 
     "facility_name": "Manufacture Plant 2",
     "facility_type": "Manufacturing plant",
     "facility_address": "1 Main Street, Zaragoza, Zaragoza, ES",
-    "timestamp": "2018-03-11T11:00:00Z"
+    "timestamp": "2018-03-11T11:00:00Z",
+    "image": "images/TomatoJuicePlant2.png",
+    "event": "Produce juice"
   }];
 
   // get the lot id from request
@@ -101,10 +124,10 @@ router.get('/trace', function(req, res) {
   } else if (lotid === "TomatoJuice1Lx12") {
     traceresult = trace2;
   }
-  console.log('lotid:' + lotid + ', result:' + traceresult + ', length:' + traceresult.length);
+  //console.log('lotid:' + lotid + ', result:' + traceresult + ', length:' + traceresult.length);
   //res.json(traceresult);
   //res.render('trace', { title: lotid});
-  res.render('trace', { title: 'Trace', "traceresult": traceresult});
+  res.render('trace', { title: 'Trace', "lotid": lotid, "traceresult": traceresult});
 });
 
 module.exports = router;
